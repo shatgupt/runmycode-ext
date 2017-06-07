5fc3ca808311b15e8f638f3ec1174bdcb227af5b
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2017-06-08 00:50:51 +0530

    fix: log not needed

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict'
 // pass message to contentscript that url has changed
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log('browser.tabs.onUpdated', changeInfo.status)
+  // console.log('browser.tabs.onUpdated', changeInfo.status)
   if (changeInfo.status === 'complete') {
     browser.tabs.sendMessage(tabId, 'pageUpdated')
   }
