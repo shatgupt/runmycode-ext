cb1b8553bdf264fa14a884e3c18ab167f406efef
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2017-06-24 13:47:55 +0530

    fix(options): cosmetics and validations

--- options.js
+++ options.js
@@ -1,22 +1,48 @@
 'use strict'
 
-const apiUrl = document.querySelector('#api-url')
-const apiKey = document.querySelector('#api-key')
-const saveBtn = document.querySelector('#save')
+const defaultAPIURL = 'https://api.runmycode.online/run'
+const $ = s => document.querySelector(s)
+const $$ = s => document.querySelectorAll(s)
+const editBtn = $('#edit')
+const error = $('#error')
+const apiUrl = $('#api-url')
+const apiKey = $('#api-key')
+const saveBtn = $('#save')
+const inputs = Array.from($$('input'))
 
-const enableSave = () => {
+const toggleInputs = () => {
+  inputs.forEach((el) => {
+    if (el.type === 'button') el.disabled = !el.disabled
+    else if (el.type === 'text') {
+      el.getAttribute('readonly') ? el.removeAttribute('readonly') : el.setAttribute('readonly', 'readonly')
+    }
+  })
+}
+
+const enableEdit = () => {
   saveBtn.value = 'Save'
-  saveBtn.disabled = false
+  toggleInputs()
 }
 
 const saveOptions = (e) => {
-  if (e) e.preventDefault()
+  if (apiUrl.value === '') apiUrl.value = defaultAPIURL
+  error.style.display = 'block'
+  if (apiUrl.value !== defaultAPIURL && apiUrl.value.indexOf('.amazonaws.com') === -1) {
+    error.textContent = `Only ${defaultAPIURL} and AWS API Gateway URLs are supported as API URL`
+    return
+  } else if (!apiKey.value && apiUrl.value === defaultAPIURL) {
+    error.textContent = 'Key cannot be empty for https://api.runmycode.online/run'
+    return
+    // key may be not required for custom RunMyCode deployment
+  }
+  error.style.display = 'none'
+
   browser.storage.local.set({
     'apiUrl': apiUrl.value,
     'apiKey': apiKey.value
   })
   saveBtn.value = 'Saved'
-  saveBtn.disabled = true
+  if (e) toggleInputs() // toggle inputs only if save from btn click
 }
 
 const restoreOptions = () => {
@@ -35,5 +61,4 @@ const restoreOptions = () => {
 
 document.addEventListener('DOMContentLoaded', restoreOptions)
 saveBtn.addEventListener('click', saveOptions)
-apiUrl.addEventListener('input', enableSave)
-apiKey.addEventListener('input', enableSave)
+editBtn.addEventListener('click', enableEdit)
