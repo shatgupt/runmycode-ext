23993af340d5aa9a251e6a2c57c884431b2caff8
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2017-08-08 22:49:44 +0530

    chore: bump version

--- build-config.js
+++ build-config.js
@@ -8,7 +8,7 @@ const config = {
       'description': 'Run code online from sites like Github, Gitlab and more',
       'author': 'Shatrughn Gupta',
       'homepage_url': 'https://runmycode.online',
-      'version': '1.5.2',
+      'version': '1.5.3',
       'icons': { '128': 'icon128.png' },
       'manifest_version': 2,
       'content_scripts': [
