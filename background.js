34e514e5df3c635158ef05abf5fb206d8f8428cd
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2018-10-21 13:58:43 -0700

    feat: optional website permission support (#11)
    Now we don't need to specify all supported websites' permission before hand.
    By default, extension will get installed with permissions for GitHub.
    User can choose to give permission to the extension on other supported sites if they require.
    
    This also allows us add support for more websites without triggering permission warnings or update blocking by Chrome store.

--- background.js
+++ background.js
@@ -1,14 +1,140 @@
+/* global browser */
 'use strict'
 
+// This locationMap is mirrored from common-utils.js.
+// Should always be synced to/from there after any change.
+const locationMap = {
+  'github.com': 'github',
+  'gist.github.com': 'github_gist',
+  'gitlab.com': 'gitlab',
+  'gitlab.com/snippets': 'gitlab_snippets',
+  'bitbucket.org': 'bitbucket',
+  'bitbucket.org/snippets': 'bitbucket_snippets',
+  'gobyexample.com': 'gobyexample',
+  'xahlee.info': 'xahlee'
+}
+
 const site = 'https://runmycode.online'
+
+const setPageActionSupported = (tabId) => {
+  browser.pageAction.show(tabId)
+  browser.pageAction.setTitle({
+    tabId: tabId,
+    title: 'Click to allow RunMyCode extension on this site.'
+  })
+  browser.pageAction.setIcon({
+    tabId: tabId,
+    path: '/icon-supported.png'
+  })
+}
+
+const setPageActionUnsupported = (tabId) => {
+  browser.pageAction.hide(tabId)
+  browser.pageAction.setTitle({
+    tabId: tabId,
+    title: 'This site is not yet supported by RunMyCode.'
+  })
+  browser.pageAction.setIcon({
+    tabId: tabId,
+    path: '/icon128.png'
+  })
+}
+
+const setPageActionActive = (tabId) => {
+  browser.pageAction.show(tabId)
+  browser.pageAction.setTitle({
+    tabId: tabId,
+    title: 'RunMyCode extension is running on this site.'
+  })
+  browser.pageAction.setIcon({
+    tabId: tabId,
+    path: '/icon-active.png'
+  })
+}
+
+const injectScripts = (tabId, domain) => {
+  browser.tabs.executeScript({
+    file: '/browser-polyfill.min.js'
+  })
+    .then(() => {
+      return browser.tabs.executeScript({
+        file: '/common-utils.js'
+      })
+    })
+    .then(() => {
+      const platform = locationMap[domain].split('_')[0]
+      return browser.tabs.executeScript({
+        file: `/platforms/${platform}.js`
+      })
+    })
+    .then(() => {
+      return browser.tabs.executeScript({
+        file: '/runmycode.js'
+      })
+    })
+    .then(() => {
+      return browser.tabs.insertCSS({
+        file: '/runmycode-panel.css'
+      })
+    })
+    .then(() => {
+      browser.tabs.sendMessage(tabId, 'pageUpdated')
+      setPageActionActive(tabId)
+    })
+}
+
+const addPermissionListener = () => {
+  browser.pageAction.onClicked.addListener((tab) => {
+    const url = tab.url.split('/')
+    const domain = url[2]
+    const permissionsToRequest = {
+      origins: [url[0] + '//' + domain + '/']
+    }
+
+    if (domain in locationMap && !domain.endsWith('github.com')) {
+      browser.permissions.contains(permissionsToRequest).then((hasPerm) => {
+        if (!hasPerm) {
+          browser.permissions.request(permissionsToRequest)
+            .then((response) => {
+              if (response) injectScripts(tab.id, domain)
+            })
+        }
+      })
+    }
+  })
+}
+
 // pass message to contentscript that url has changed
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    browser.tabs.sendMessage(tabId, 'pageUpdated')
+  setPageActionUnsupported(tabId)
+  if (changeInfo.status !== 'complete') return
+  const url = tab.url.split('/')
+  const domain = url[2]
+  const permissionsToRequest = {
+    origins: [url[0] + '//' + domain + '/']
+  }
+
+  if (domain in locationMap) {
+    if (domain.endsWith('github.com')) {
+      // We have, by default, permission for GitHub
+      setPageActionActive(tabId)
+      browser.tabs.sendMessage(tabId, 'pageUpdated')
+    } else {
+      browser.permissions.contains(permissionsToRequest).then((hasPerm) => {
+        if (hasPerm) {
+          // Already got permission, inject required scripts and css
+          injectScripts(tabId, domain)
+        } else {
+          // Supported but no permission yet
+          setPageActionSupported(tabId)
+          addPermissionListener()
+        }
+      })
+    }
   }
 })
 
-// Open the dashboard page on install so that extension can be configured automatically
+// Open the dashboard page on install so that the extension can configure itself
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     browser.tabs.create({
--- platforms/bitbucket.js
+++ platforms/bitbucket.js
@@ -1,3 +1,4 @@
+/* global MutationObserver, CustomEvent */
 'use strict';
 
 ((window, document) => {
