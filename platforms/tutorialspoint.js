2f6f02b8840f2b22f91434979e759f831b828d91
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2018-12-14 02:05:09 -0700

    fix: PHP7 support

--- platforms/tutorialspoint.js
+++ platforms/tutorialspoint.js
@@ -23,6 +23,7 @@
     'python3': 'python3',
     'ruby': 'ruby',
     'php': 'php',
+    'php7': 'php',
     'java': 'java',
     'java8': 'java',
     'go': 'go',
@@ -33,18 +34,19 @@
 
   // bail out early if its not a supported language page
   tp.getPage = () => {
-    if (pageLang && $('a.demo')) return 'show'
+    // pre.tryit for PHP7
+    if (pageLang && ($('a.demo') || $('pre.tryit'))) return 'show'
   }
 
   // Java public classes require filename to be same as class name.
   // Try best to extract it
-  tp.getFileName = (btn) => {
+  const getFileName = (demoBtn) => {
     if (pageLang === 'java') {
       // check if previous h3 element has filename
-      const prevText = btn.previousElementSibling.textContent
+      const prevText = demoBtn.previousElementSibling.textContent
       if (prevText.includes('.java')) return prevText
       // try to extract from code, gets the first public class
-      const src = btn.nextElementSibling.textContent
+      const src = demoBtn.nextElementSibling.textContent
       const publicClassRegex = /public class (\w+)/g
       const match = publicClassRegex.exec(src)
       if (match !== null) return match[1] + '.java'
@@ -53,27 +55,40 @@
     return pageLang + '.' + langExtMap[pageLang]
   }
 
+  const buildRunButton = (demoBtn) => {
+    return buildDomElement(
+      ['a',
+        {
+          'class': 'demo runmycode-popup-runner',
+          'href': '#',
+          'style': 'right: 115px;',
+          'data-filename': getFileName(demoBtn),
+          'data-lang': pageLang
+        },
+        'Run Here'
+      ]
+    )
+  }
+
   tp.pages = {}
   tp.pages.show = {
     getCodeContainer: (openRunnerBtn) => openRunnerBtn.nextElementSibling,
     hasSupportedLang: () => true, // always true for show page
     injectRunButtons: () => {
-      $$('a.demo').forEach((btn) => {
-        if (btn.nextElementSibling.classList.contains('runmycode-popup-runner')) return
-        btn.parentNode.insertBefore(
-          buildDomElement(
-            ['a',
-              {
-                'class': 'demo runmycode-popup-runner',
-                'href': '#',
-                'style': 'right: 115px;',
-                'data-filename': tp.getFileName(btn),
-                'data-lang': pageLang
-              },
-              'Run Here'
-            ]
-          ),
-          btn.nextElementSibling
+      $$('a.demo').forEach((demoBtn) => {
+        if (demoBtn.nextElementSibling.classList.contains('runmycode-popup-runner')) return
+        demoBtn.parentNode.insertBefore(
+          buildRunButton(demoBtn),
+          demoBtn.nextElementSibling
+        )
+      })
+
+      // PHP7 has tryit button inside pre
+      $$('pre.tryit').forEach((pre) => {
+        if (pre.previousElementSibling.classList.contains('runmycode-popup-runner')) return
+        pre.parentNode.insertBefore(
+          buildRunButton(pre),
+          pre
         )
       })
     },
